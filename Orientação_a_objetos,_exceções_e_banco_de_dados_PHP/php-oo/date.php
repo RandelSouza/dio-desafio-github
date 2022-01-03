8e605555ba3f77d087d296000ecccf780d4b765b
randelsouza <randelsouza88@gmail.com>
2022-01-03 13:34:30 -0300

    Finalizando primeiro item da segunda parte - Trabalhando com datas e execoes em php - Aprendendo sobre a classe DateTime e manipulando datas e horas

---
+++ Orientação_a_objetos,_exceções_e_banco_de_dados_PHP/php-oo/date.php
@@ -0,0 +1,13 @@
+<?php
+
+// echo date('d/m/y') . PHP_EOL;
+
+$data = new DateTime();
+
+$intervalo = new DateInterval('P5DT10H50M');
+
+//$data->add($intervalo);
+$data->sub($intervalo);
+
+var_dump($data); 
+//echo $data->format('d-m-Y H:i:s') . PHP_EOL;
